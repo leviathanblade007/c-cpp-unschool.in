9b362d779b2404a34df3ed7d07e835094c03ed2a
leviathanblade007 <54022747+leviathanblade007@users.noreply.github.com>
2020-07-22 12:47:08 +0530

    Add files via upload
    These are the answer to the assignment : OPERATORS

---
+++ sumsubmultdiv program.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("ENTER 2 NOS\n");
+scanf("%d %d",&a,&b);
+printf("SUM OF %d AND %d IS %d",a,b,(a+b));
+printf("DIFFERENCE OF %d AND %d IS %d",a,b,(a-b));
+printf("MULTIPLICATION OF %d AND %d IS %d",a,b,(a*b));
+printf("DIVISION OF %d AND %d IS %d",a,b,(a/b));
+}
